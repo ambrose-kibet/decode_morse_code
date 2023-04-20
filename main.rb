b578f53ce0a761a820dc7772bb0f2e7851a22720
ambrose-kibet <ambrosekibet36@gmail.com>
2023-04-20 12:16:15 +0300

    fix linters

--- main.rb
+++ main.rb
@@ -1,81 +1,159 @@
 def decode_char(char)
   morse_code = {
 
+
+
     '.-' => 'A',
+
+
 
     '-...' => 'B',
 
+
+
     '-.-.' => 'C',
+
+
 
     '-..' => 'D',
 
+
+
     '.' => 'E',
+
+
 
     '..-.' => 'F',
 
+
+
     '--.' => 'G',
+
+
 
     '....' => 'H',
 
+
+
     '..' => 'I',
+
+
 
     '.---' => 'J',
 
+
+
     '-.-' => 'K',
+
+
 
     '.-..' => 'L',
 
+
+
     '--' => 'M', '
+
+
 
       -.' => 'N',
 
+
+
     '---' => 'O',
+
+
 
     '.--.' => 'P',
 
+
+
     '--.-' => 'Q',
+
+
 
     '.-.' => 'R',
 
+
+
     '...' => 'S',
+
+
 
     '-' => 'T',
 
+
+
     '..-' => 'U',
+
+
 
     '...-' => 'V',
 
+
+
     '.--' => 'W',
+
+
 
     '-..-' => 'X',
 
+
+
     '-.--' => 'Y',
+
+
 
     '--..' => 'Z',
 
+
+
     '-----' => '0',
+
+
 
     '.----' => '1',
 
+
+
     '..---' => '2',
+
+
 
     '...--' => '3',
 
+
+
     '....-' => '4',
+
+
 
     '.....' => '5',
 
+
+
     '-....' => '6',
+
+
 
     '--...' => '7',
 
+
+
     '---..' => '8',
+
+
 
     '----.' => '9',
 
+
+
     '  ' => ' '
 
+
+
   }
+
+
 
   morse_code[char]
 end
@@ -85,17 +163,25 @@ end
 puts decode_char('-...')
 
 
-def decode_word (word) 
- decoded_word = ""
- word.split(" ").each{|item|decoded_word.concat decode_char(item)}
- puts decoded_word
+
+def decode_word(word)
+  decoded_word = ''
+
+  word.split.each { |item| decoded_word.concat decode_char(item) }
+
+  puts decoded_word
 end
-decode_word ("-- -.--")
+
+decode_word('-- -.--')
+
 
 
 def decode(sentence)
   sentence.split('  ').each { |word| decode_word(word) }
 end
+
 decode_char('.-')
+
 decode_word('-- -.--')
+
 decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
